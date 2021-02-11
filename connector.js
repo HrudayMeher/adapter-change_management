33bddaf3b600b7a499c61801afb45851368e9417
HrudayMeher <hruday.meher@gmail.com>
2021-02-11 17:25:50 +0000

    Final Project

--- connector.js
+++ connector.js
@@ -5,7 +5,7 @@ const validResponseRegex = /(2\d\d)/;
 const options = {
   url: 'https://dev62247.service-now.com/',
   username: 'admin',
-  password: 'GBiwDoebCV84',
+  password: '$ENCa4c88e3d607af447a9ed0f45d482691f8582258ca8d2419d57088d1d',
   serviceNowTable: 'change_request'
 };
 /**
